6f376b8fa24dea8ccee98d707a841cad8b83ec93
wrenaustinc <70230511+wrenaustinc@users.noreply.github.com>
2022-07-22 18:18:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ function moveCardWhenIssueMilestoned(apiKey, apiToken, boardId) {
   const departureListId = process.env['TRELLO_DEPARTURE_LIST_ID'];
   const destinationListId = process.env['TRELLO_DESTINATION_LIST_ID'];
   const issue = github.context.payload.issue
-  const issue_number = issue.number
+  const issue_number = issue.number;
   const url = pullRequest.html_url;
   const reviewers = pullRequest.requested_reviewers.map(reviewer => reviewer.login);
 
